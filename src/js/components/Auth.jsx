457d9fb01bf72c365b5090ca45783b917e61af03
JessMitch <jessicamitch42@gmail.com>
2016-08-16 15:09:37 +0000

    reverting Auth.jsx to last night

--- src/js/components/Auth.jsx
+++ src/js/components/Auth.jsx
@@ -4,10 +4,6 @@ var Home = require("./Home");
 var LoggedIn = require("./LoggedIn");
 import Auth0Lock from 'auth0-lock';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 var Auth = React.createClass({
   getInitialState: function() {
     return {
@@ -56,7 +52,6 @@ var Auth = React.createClass({
   render: function() {
     if (this.state.profile) {
       var userObject = {
-<<<<<<< HEAD
         username: ,
         email:,
         city: "montreal",
@@ -64,16 +59,6 @@ var Auth = React.createClass({
         profilepic:,
         typeOfLogin: 
       };
-=======
-        username: this.state.profile.nickname,
-        city: "Montreal",
-        email: "",
-        nickname: this.state.profile.name,
-        profilepic: this.state.profile.picture,
-        typeOfLogin: this.state.profile.identities[0].provider
-      };
-      console.log(userObject)
->>>>>>> master
       return (<LoggedIn profile={this.state.profile} lock={this.state.lock} idToken={this.state.idToken} />);
     }
     else {
