05c04db3459c78cfdde46ebe2f6aa0637c662037
Maxime Martin <maxime.martin123@gmail.com>
2016-08-16 20:07:43 +0000

    createareview

--- src/js/routes.js
+++ src/js/routes.js
@@ -9,6 +9,7 @@ var App = require("./components/App");
 var Auth = require("./components/Auth");
 var Home = require("./components/Home");
 var Loggedin = require("./components/Loggedin");
+var Createareview = require("./components/createareview");
 
 import Foundation from 'react-foundation';
 
@@ -18,6 +19,7 @@ var routes = (
             <Route path="auth" component={Auth}></Route>
             <Route path="home" component={Home}></Route>
             <Route path="loggedin" component={Loggedin}></Route>
+            <Route path="createareview" component={Createareview}></Route>
         </Route>
     </Router>
 );
