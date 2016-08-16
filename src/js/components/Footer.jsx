f974cd84930be9676c812abd35986b2f4b815564
JessMitch <jessicamitch42@gmail.com>
2016-08-16 20:57:57 +0000

    react foundation added, Header and Footer updated

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,10 +1,40 @@
 var React = require('react');
+import {Menu, MenuItem} from 'react-foundation';
+
 //find ouy how to render autho component
 var Footer = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Footer</h1>
+      <div className="menu-vertical-example">
+        <Menu isVertical>
+          <MenuItem><a>One</a></MenuItem>
+          <MenuItem><a>Two</a></MenuItem>
+          <MenuItem><a>Three</a></MenuItem>
+          <MenuItem><a>Four</a></MenuItem>
+        </Menu>
+      </div>
+      <div className="footer">
+        <div className="footer-profiles">
+        <ul>
+          <li>Browse Profiles</li>
+          <li>Create/View Your Profile</li>
+        </ul>
+        </div>
+        <div className="footer-about-us">
+        <ul>
+          <li>How It Works</li>
+          <li>Our Vision</li>
+          <li>FAQ</li>
+        </ul>
+        </div>
+        <div className="footer-connect">
+        <ul>
+          <li>Contact Us</li>
+          <li>Careers</li>
+        </ul>
+        </div>
+      </div>
       </div>
     );
   }
--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,10 +1,10 @@
 var React = require('react');
-
+// var img = require("../../public/img/hairphoto.jpeg");
 var Header = React.createClass({
   render: function() {
     return (
       <div>
-      <img src="../img/hairphoto" />
+        <img src="/img/hairphoto.jpeg"/>
         <h1>Header</h1>
       </div>
     );
