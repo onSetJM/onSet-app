6acf482528c14ceffa430cd2699b28783bd3770d
JessMitch <jessicamitch42@gmail.com>
2016-08-18 07:41:29 +0000

    updated auth, still need to access user profile

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,3 +1,4 @@
+/*global localStorage */
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
@@ -6,31 +7,44 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 
+import AuthService from './utils/AuthService';
+
 var App = require("./components/App");
 var Home = require("./components/Home");
-var Loggedin = require("./components/Loggedin");
 var Createareview = require("./components/Createareview");
 var Createartistprofile = require("./components/Createartistprofile");
 var Profile = require("./components/Profile");
-
-var SearchBar = require("./components/SearchBar");
-var SearchResults = require("./components/SearchResults");
+var Login = require("./components/Login");
+var Logout = require("./components/Logout");
 var Header = require("./components/Header");
 var Footer = require("./components/Footer");
 
+var SearchBar = require("./components/SearchBar");
+var SearchResults = require("./components/SearchResults");
 var Reviews = require("./components/Reviews");
 
+var auth = new AuthService('pQZynj9aeB6FgPoKihk7HluGGlLYwqWR', 'onset.auth0.com');
 
+var requireAuth = function(nextState, replace) {
+    if (!auth.loggedIn()) {
+        localStorage.setItem('last_url', window.location.pathname);
+        replace({ pathname: '/login' + window.location.hash})
+    }
+}
 
 var routes = (
-    <Router history={ReactRouter.browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Home}/>
+    <Router history={ReactRouter.browserHistory}> 
+        <Route path="/" component={App} auth={auth}>
+            <IndexRoute component={Home} onEnter={requireAuth}/>
+            <Route component={Header} />
+            <Route component={Footer} />
+            <Route path="login" component={Login} />
+            <Route path="logout" component={Logout} />
             <Route component={SearchBar}></Route>
-            <Route path="loggedin" component={Loggedin}></Route>
-            <Route path="createareview" component={Createareview}></Route>
-            <Route path="createartistprofile" component={Createartistprofile}></Route>
+            
             <Route path="search" component={SearchResults}></Route>
+            <Route path="createareview" component={Createareview} onEnter={requireAuth} ></Route>
+            <Route path="createartistprofile" component={Createartistprofile}></Route>
             <Route path="profile" component={Profile}></Route>
             <Route path="reviews" component={Reviews}></Route>
         </Route>
