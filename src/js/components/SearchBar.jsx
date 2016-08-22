0ccbf5f8396f7b5f0e8defd15eaf9959118a3168
Maxime Martin <maxime.martin123@gmail.com>
2016-08-22 22:17:19 +0000

    search bar style + fix the form

--- src/js/components/SearchBar.jsx
+++ src/js/components/SearchBar.jsx
@@ -7,13 +7,15 @@ var Link = require("react-router").Link;
 
 var SearchBar = React.createClass({
     _handleSearch: function () {
-        history.push(`/search/${this.refs.category.value}/${this.refs.city.value}/searchresults`);
+        history.push(`/search/${this.refs.category.value}/${this.refs.city.value}/${this.refs.filter.value}`);
     },
     render: function(){
+        var children = this.props.children && React.cloneElement(this.props.children, {searchObj: this.state.searchObj });
         return (
+            
             <div className="homepagesearchbar">
                     
-                    <select  className="form-control" ref='category'>
+                    <select  ref='category' defaultValue="hairstylist">
                         <option value="hairstylist">Hairstylist </option>
                          <option value="make-up Artist">Make-up Artist</option>
                          <option value="colorist">Colorist</option>
@@ -21,12 +23,22 @@ var SearchBar = React.createClass({
                          <option value="dualist">Dualist</option>
                           <option value="nailartist">Nail Artist</option>
                     </select>
-
                 <input type="search" ref="city" className="searchinput eqHeight" placeholder="City" />
+                    <select ref="filter">
+                     <option value="" disabled selected>Filter by </option>
+                        <option value="profileScore">Best profile scores </option>
+                         <option value="totalReviews">Total number of reviews</option>
+                          <option value="profileCreateAt"> Latest profiles </option>
+                     </select>    
                 <button type="button" onClick={this._handleSearch} className="btn btn-secondary btn-sm eqHeight">Search</button>
             </div>
+          
         );
     }
 });
 
 module.exports = SearchBar;
+/*
+
+
+*/
