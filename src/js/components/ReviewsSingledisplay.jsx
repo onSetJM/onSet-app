852ece30bf1ca24d52aa67285c05aac46de0f7b3
Maxime Martin <maxime.martin123@gmail.com>
2016-08-23 15:08:13 +0000

    reviews stars

--- src/js/components/ReviewsSingledisplay.jsx
+++ src/js/components/ReviewsSingledisplay.jsx
@@ -7,14 +7,26 @@ var Link = require("react-router").Link;
 var ReviewsSingledisplay = React.createClass({
     render: function() {
         return (
-            <div className="singleReview" key={this.props.review.reviewId}>
+            <div className="singleReviewContainer" key={this.props.review.reviewId}>
+                <div className="reviewHeader">
                     <div className="reviewerUsername">Reviewer: <span className="review-var">{this.props.review.reviewerusername}</span></div>
+                </div>
+                <div className="reviewBody">
                     <div className="reviewScore">Score: <span className="review-var">{this.props.review.reviewScore}</span></div>
                     <div className="reviewText">Review: <span className="review-var">{this.props.review.reviewText}</span></div>
                     <div className="reviewpText">Date: <span className="review-var">{this.props.review.reviewCreatedAt}</span></div>
+                </div>
+                <div className="reviewfooter">
+                </div>
             </div>
+            
         );
     }
 });
 
 module.exports = ReviewsSingledisplay;
+
+/*
+
+
+*/
